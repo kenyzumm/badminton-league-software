b8826e9282f6a5bb2f8f3e86e8deab1f4db28ae5
Jan Pabich <janekp923@gmail.com>
2025-05-30 23:49:31 +0200

    login.php  main.php

--- login.php
+++ login.php
@@ -1,27 +1,31 @@
-<!DOCTYPE html>
-<head>
-    <meta charset="UTF-8">
-    <title>Login validation</title>
-    <link rel="stylesheet" href="css/style.css" type="text/css"/>
-</head>
-<body>
-    <div class="container">
-        <h2>Dane logowania:</h2>
 <?php
     require_once "db_conf.php";
-
-    $connection = @new mysqli($host, $db_user, $db_password, $db_name);
-    if($connection->connect_errno!=0){
-        echo "error";
+    $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);    
+    if($polaczenie->connect_errno!=0){
+        echo "Error: ".$polaczenie->connect_errno;
     }
     else{
         $login = $_POST['login'];
         $haslo = $_POST['haslo'];
+        
+        $sql = "SELECT * FROM users WHERE user ='$login' AND pass = '$haslo'";
+        if($result = @$polaczenie->query($sql))
+        {
+            $ilu_userow = $result->num_rows;
+            if($ilu_userow>0){
+                $wiersz =$result->fetch_assoc();
+                $user=$wiersz['user'];
 
-        echo '<div class="dane">Login: '.$login.'</div>';
-        echo '<div class="dane">Haslo: '.$haslo.'</div>';
+                $result->free_result();
+                header('location: main.php');
+            }else{
+                echo "nie ma takiego usera";
+
+            }
+        }
+        
+        $polaczenie->close();
+
+
     }
 ?>
-
-    </div>
-</body>
