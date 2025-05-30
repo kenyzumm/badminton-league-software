685521ec55d71c07de8fc2b409a063ce57d8ecdf
Jan Pabich <janekp923@gmail.com>
2025-05-30 23:58:24 +0200

    login.php  main.php

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     require_once "db_conf.php";
     $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);    
     if($polaczenie->connect_errno!=0){
@@ -14,10 +15,10 @@
             $ilu_userow = $result->num_rows;
             if($ilu_userow>0){
                 $wiersz =$result->fetch_assoc();
-                $user=$wiersz['user'];
+                $_SESSION['user']=$wiersz['user'];
 
                 $result->free_result();
-                header('location: main.php');
+                header('Location: main.php');
             }else{
                 echo "nie ma takiego usera";
 
