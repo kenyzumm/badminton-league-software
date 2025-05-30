f182d54b30c4811ecedead3ef0cf3e396db3ddc2
Jan Pabich <janekp923@gmail.com>
2025-05-31 00:28:50 +0200

    main css

--- main.php
+++ main.php
@@ -7,7 +7,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/style.css" type="test/css"/>
+    <link rel="stylesheet" href="css/style.css" type="text/css"/>
+
 
 
 </head>
